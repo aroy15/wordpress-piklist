8b7c57e13f52cc096699acb768069486faf44a64
aroy15 <anjonroy57@gmail.com>
2023-12-08 15:57:48 +0600

    Dependancy & condition applied

--- piklist/parts/meta-boxes/dependency.php
+++ piklist/parts/meta-boxes/dependency.php
@@ -12,5 +12,34 @@ piklist('field', array(
         'red' => __('Red', 'twentyseventeen'),
         'green' => __('Green', 'twentyseventeen'),
         'cyan' => __('Cyan', 'twentyseventeen'),
+        'other' => __('Other', 'twentyseventeen'),
     )
 ));
+piklist('field', array(
+    'type' => 'select',
+    'field' => 'colors2',
+    'label' => __('Select a secondary color', 'twentyseventeen'),
+    'choices' => array(
+        'red' => __('Red', 'twentyseventeen'),
+        'green' => __('Green', 'twentyseventeen'),
+        'cyan' => __('Cyan', 'twentyseventeen'),
+        'other' => __('Other', 'twentyseventeen'),
+    )
+));
+piklist('field', array(
+    'type' => 'colorpicker',
+    'field' => 'othercolor',
+    'label' => __('Select color', 'twentyseventeen'),
+    'value' => '#ff0000',
+    'conditions'=> array(
+        'relation' => 'or',
+        array(
+            'field' =>  'colors',
+            'value' => 'cyan'
+        ),
+        array(
+            'field' =>  'colors2',
+            'value' => 'green'
+        )
+    )
+)); 
